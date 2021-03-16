637f8733f0cdb4cfede9b299cbb51cc7ac475919
imramzan <77074717+imramzan@users.noreply.github.com>
2021-03-16 15:30:16 -0400

    PyPoll Data analysis

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,77 +5,69 @@ Created on Thu Mar  4 12:11:39 2021
 @author: ramza
 """
 import os
-import zipfile
 import csv
 
-zf = zipfile.ZipFile("Raw_Data/election_data.zip")
-csvpath = os.path.join(zf)
+# Set the path for the CSV file in PyPollcsv
 
-with open(csvpath, newline="") as csvfile:
-    csv_reader =csv.reader(csvfile, delimiter=",")
-    csv_headers = next(csv_reader, None)
-    #print (f"CSV Header: {csv_headers}"
+PyPoll = os.path.join("PyPoll/Raw_Data/election_data.csv")
 
-    # print heading 
-    print ("Election Results")
-    print ("-----------------------")
+# Create the lists to store data and initialize
 
+count = 0
+candidatelist = []
+unique_candidate = []
+vote_count = []
+vote_percent = []
 
-    # Variables
-    candidate_list = []
-    count_votes = 0
+# Open the CSV using the set path PyPoll
+
+with open(PyPoll, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvreader)
     
-    # Loop through csv_reader
-    for row in csv_reader:
-        # if value in Candidate column is not in candidate_list,
-        # append to candidate_list
-        if row[2] not in candidate_list:
-            count_votes += 1
-            candidate_list.append(row[2])
-        else: # if not in list, just add the vote count
-            count_votes += 1
-
-        # print (len(candidate_list)) = 4
+    # Loop through csvreader
+    for row in csvreader:
+        # Count the total number of votes
+        count = count + 1
+        # Set the candidate names to candidatelist
+        candidatelist.append(row[2])
+        # Create a set from the candidatelist to get the unique candidate names
+    for x in set(candidatelist):
+        unique_candidate.append(x)
+        # counting total number of votes per candidate (y)
+        y = candidatelist.count(x)
+        vote_count.append(y)
+        # z is the percent of total votes per candidate
+        z = round((y/count)*100,2)
+        vote_percent.append(z)
+        
+    winning_vote_count = max(vote_count)
+    winner = unique_candidate[vote_count.index(winning_vote_count)]
     
-    # count votes per candidate
-    i = 0 
-    num_candidates = len(candidate_list)
-    candidate_votes = [0]*num_candidates
+print("-------------------------")
+print("Election Results")   
+print("-------------------------")
+print("Total Votes :" + str(count))    
+print("--------------------------------------------")
+print("Votes per candidate-------------------------")
+for i in range(len(unique_candidate)):
+            print(unique_candidate[i] + ": " + str(vote_percent[i]) +"% (" + str(vote_count[i])+ ")")
+print("-------------------------")
+print('Maximum votes obtained by a single candidate were '+(str(winning_vote_count)))
+print("The winner is: " + winner)
+print("-------------------------")
 
-    for i in range(num_candidates):
-        if row[2] == candidate_list[i]:
-            candidate_votes[i] += 1
+# Print to a text file: election_results.txt
+# Output perhaps needs to be rounded to 3 decimal points. Leaving that formatting out for now) 
 
-    most_vote = max(candidate_votes)
-    vote_index = candidate_votes.index(most_vote)
-    winner = candidate_list[vote_index]
-
-    #print (winner)
-
-    # percent per candidate
-    j = 0
-    percent_of_votes = []
-
-    for j in range(num_candidates):
-        percent = round(candidate_votes[j]/count_votes * 100,2)
-        percent_of_votes.append(percent)
-
-
-    print ("Election Results")
-    print ("-----------------------------")
-    print (f"Total Votes: {count_votes}")
-    print ("-----------------------------")
-    print (f"Name: {candidate_list} {percent_of_votes}, {candidate_votes}")
-    print ("-----------------------------")
-    print (f"Winner: {winner}")
-
-exportpath = ("Results.txt")
-with open(exportpath, "w") as textfile:
-        textfile.write("Election Results")
-        textfile.write("-----------------------------")
-        textfile.write(f"Total Votes: {count_votes}")
-        textfile.write("-----------------------------")
-        textfile.write(f"Name: {candidate_list} {percent_of_votes}, {candidate_votes}")
-        textfile.write("-----------------------------")
-        textfile.write(f"Winner: {winner}")
-    
+with open('election_results.txt', 'w') as text:
+    text.write("Election Results\n")
+    text.write("---------------------------------------\n")
+    text.write("Total Votes: " + str(count) + "\n")
+    text.write("----------------------------------------------------------\n")
+    text.write("Votes per candidate---------------------------------------\n")
+    for i in range(len(set(unique_candidate))):
+        text.write(unique_candidate[i] + ": " + str(vote_percent[i]) +"% (" + str(vote_count[i]) + ")\n")
+    text.write("---------------------------------------\n")
+    text.write("The winner is: " + winner + "\n")
+    text.write("---------------------------------------\n")
